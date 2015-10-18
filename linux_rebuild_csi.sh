983605bc8042dcc6cd71b3fa330643c023c9e49a
skarlsson <svante.karlsson@csi.se>
2015-10-18 16:45:17 +0200

    added version variables

--- linux_rebuild_csi.sh
+++ linux_rebuild_csi.sh
@@ -1,3 +1,9 @@
+export BOOST_VERSION=1_59_0
+export BOOST_VERSION_DOTTED=1.59.0
+export JOYENT_HTTP_VERSION=2.3
+export SQLPP11_VERSION=0.33
+export C_ARES_VERSION=1.10.0
+export CURL_VERSION=7.42.1
 export AVRO_DIR=avro/lang/c++
 
 pushd $AVRO_DIR
@@ -9,11 +15,6 @@ make -j4
 sudo make install
 cd ..
 popd
-
-cd postgres-asio
-git pull
-bash -e build_linux.sh
-cd ..
 
 cd json-spirit
 git pull
@@ -31,6 +32,11 @@ bash -e build_linux.sh
 cd ..
 
 cd csi-kafka
+git pull
+bash -e build_linux.sh
+cd ..
+
+cd postgres-asio
 git pull
 bash -e build_linux.sh
 cd ..
