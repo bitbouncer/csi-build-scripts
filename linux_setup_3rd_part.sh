af8b5d5b2afe0e7a499b7a42ad7bbff6d96da086
skarlsson <svante.karlsson@csi.se>
2015-11-26 00:21:33 +0100

    initial support for snappy on linux

--- linux_setup_3rd_part.sh
+++ linux_setup_3rd_part.sh
@@ -1,3 +1,4 @@
+export SNAPPY_VERSION=1.1.3
 export BOOST_VERSION=1_59_0
 export BOOST_VERSION_DOTTED=1.59.0
 export JOYENT_HTTP_VERSION=2.3
@@ -12,6 +13,10 @@ rm -f c-ares-$C_ARES_VERSION.tar.gz
 wget http://curl.haxx.se/download/curl-$CURL_VERSION.tar.gz
 tar xvf curl-$CURL_VERSION.tar.gz
 rm -f curl-$CURL_VERSION.tar.gz
+
+wget --no-check-certificate https://github.com/google/snappy/releases/download/$SNAPPY_VERSION/snappy-$SNAPPY_VERSION.tar.gz  -Osnappy-$SNAPPY_VERSION.tar.gz
+tar xvf snappy-$SNAPPY_VERSION.tar.gz
+rm -f snappy-$SNAPPY_VERSION.tar.gz
 
 #RIGHT NOW WE ARE USING OWN AVRO BRANCH
 #wget ftp://ftp.sunet.se/pub/www/servers/apache/dist/avro/avro-$AVRO_VERSION/cpp/avro-cpp-$AVRO_VERSION.tar.gz
@@ -29,8 +34,6 @@ rm -f sqlpp11-v$SQLPP11_VERSION.tar.gz
 wget http://sourceforge.net/projects/boost/files/boost/$BOOST_VERSION_DOTTED/boost_$BOOST_VERSION.tar.gz/download -Oboost_$BOOST_VERSION.tar.gz
 tar xf boost_$BOOST_VERSION.tar.gz
 rm -f boost_$BOOST_VERSION.tar.gz
-
-git clone https://github.com/google/snappy.git
 
 cd boost_$BOOST_VERSION
 ./bootstrap.sh
