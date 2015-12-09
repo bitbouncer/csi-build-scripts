e45b1dc1df568ebb54b7eca2f8b8a44e7115c49e
skarlsson <svante.karlsson@csi.se>
2015-12-09 03:44:01 +0100

    typo

--- windows_x64_vc12_rebuild_csi.bat
+++ windows_x64_vc12_rebuild_csi.bat
@@ -1,26 +1,5 @@
-REM need separate installation of git, nasm, active perl & visual studio
-REM make sure that active perl is before any other perl - git's for example in PATH
-
 set VISUALSTUDIO_VERSION=12.0
 set VISUALSTUDIO_VERSION_MAJOR=12
-set BOOST_VERSION=1_59_0
-set BOOST_VERSION_DOTTED=1.59.0
-set OPEN_SSL_VERSION=openssl-1.0.1j
-
-REM set C_ARES_VERSION=1.10.0
-REM set CARES_DIR=c-ares-%C_ARES_VERSION%
-set CARES_DIR=c-ares-master
-
-set CURL_VERSION=7.42.1
-set ZLIB_VERSION=1.2.8
-
-REM set AVRO_VERSION=1.7.7
-REM set AVRO_DIR=avro-cpp-%AVRO_VERSION%
-set AVRO_DIR=avro\lang\c++ 
-
-set LIBEVENT_VERSION=2.0.21
-set PTHREAD_VERSION=2-9-1
-set JOYENT_HTTP_VERSION=2.3
 
 call "C:\Program Files (x86)\Microsoft Visual Studio %VISUALSTUDIO_VERSION%\VC\vcvarsall.bat" amd64
 
