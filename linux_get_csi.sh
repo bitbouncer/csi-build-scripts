facaf5c09fe804fe14cc899a1cc33f98e1071bd7
skarlsson <svante.karlsson@csi.se>
2016-01-06 01:25:32 +0100

    added rm before clone

--- linux_get_csi.sh
+++ linux_get_csi.sh
@@ -1,15 +1,30 @@
+rm -rf avro
 git clone https://github.com/bitbouncer/avro
 mkdir avro/lang/c++/avro 
 cp -r avro/lang/c++/api/* avro/lang/c++/avro
 
+rm -rf postgres-asio
 git clone https://github.com/bitbouncer/postgres-asio
-git clone https://github.com/bitbouncer/csi-http.git
-git clone https://github.com/bitbouncer/csi-avro-utils
-git clone https://github.com/bitbouncer/csi-kafka
-git clone https://github.com/bitbouncer/json-spirit
-git clone https://github.com/bitbouncer/csi-samples
-git clone https://github.com/bitbouncer/kafka2influx
-git clone https://github.com/bitbouncer/phoebe
 
+rm -rf csi-http
+git clone https://github.com/bitbouncer/csi-http.git
+
+rm -rf csi-avro-utils
+git clone https://github.com/bitbouncer/csi-avro-utils
+
+rm -rf csi-kafka
+git clone https://github.com/bitbouncer/csi-kafka
+
+rm -rf json-spirit
+git clone https://github.com/bitbouncer/json-spirit
+
+rm -rf csi-samples
+git clone https://github.com/bitbouncer/csi-samples
+
+rm -rf kafka2influx
+git clone https://github.com/bitbouncer/kafka2influx
+
+rm -rf phoebe
+git clone https://github.com/bitbouncer/phoebe
 
 
