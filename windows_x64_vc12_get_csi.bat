486a746a945bcbd9f7f22dacdd506e448cbbc15d
saka <svante.karlsson@csi.se>
2016-01-11 07:07:59 +0100

    removed official avro until it's a real 1.8

--- windows_x64_vc12_get_csi.bat
+++ windows_x64_vc12_get_csi.bat
@@ -2,12 +2,12 @@ git clone https://github.com/bitbouncer/avro.git
 mkdir avro\lang\c++\avro
 xcopy /s /y avro\lang\c++\api\* avro\lang\c++\avro
 
-wget --no-check-certificate https://dist.apache.org/repos/dist/dev/avro/avro-1.8.0-rc1/avro-src-1.8.0.tar.gz -Oavro-src-1.8.0.tar.gz
-gunzip avro-src-1.8.0.tar.gz
-tar xf avro-src-1.8.0.tar
-del avro-src-1.8.0.tar
-mkdir avro-src-1.8.0\lang\c++\avro
-xcopy /s /y avro-src-1.8.0\lang\c++\api\* avro-src-1.8.0\lang\c++\avro
+REM wget --no-check-certificate https://dist.apache.org/repos/dist/dev/avro/avro-1.8.0-rc1/avro-src-1.8.0.tar.gz -Oavro-src-1.8.0.tar.gz
+REM gunzip avro-src-1.8.0.tar.gz
+REM tar xf avro-src-1.8.0.tar
+REM del avro-src-1.8.0.tar
+REM mkdir avro-src-1.8.0\lang\c++\avro
+REM xcopy /s /y avro-src-1.8.0\lang\c++\api\* avro-src-1.8.0\lang\c++\avro
 
 git clone https://github.com/bitbouncer/postgres-asio.git
 git clone https://github.com/bitbouncer/csi-kafka.git
